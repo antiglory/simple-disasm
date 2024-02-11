ff4a73056ac6e5cb1d9ed041c289082259538ca4
dezska <104264903+antiglory@users.noreply.github.com>
2024-02-11 14:59:32 -0300

    Update main.h

--- source/include/headers/main.h
+++ source/include/headers/main.h
@@ -1,13 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "../functions/adi.c"
+#include "../functions/intf.c"
+#include "../functions/modules/disasm.c"
+
 // prototypes
 int main();
 void disasm(uint8_t *buffer, int offset);
-
-#include "../functions/adi.c"
-#include "../functions/intf.c"
-
-#include "../functions/modules/disasm.c"
 
 #endif
